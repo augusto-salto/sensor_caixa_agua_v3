89ce43eb047ef711f0193eda3b73a2ef570ad124
augusto-salto <69981764+augusto-salto@users.noreply.github.com>
2022-02-26 16:08:21 -0300

    main

--- lib/update_firmware/update_firmware.h
+++ lib/update_firmware/update_firmware.h
@@ -13,8 +13,17 @@
 //#define URL_fw_Version "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/bin_version.txt"
 //#define URL_fw_Bin     "https://raw.githubusercontent.com/programmer131/ESP8266_ESP32_SelfUpdate/master/esp32_ota/fw.bin"
 
-#define URL_fw_Version "https://raw.githubusercontent.com/augusto-salto/updateSensorNivel/main/bin_version.txt"
-#define URL_fw_Bin "https://raw.githubusercontent.com/augusto-salto/updateSensorNivel/main/fw.bin"
+// FUNCIONAL --> #define URL_fw_Version "https://raw.githubusercontent.com/augusto-salto/updateSensorNivel/main/bin_version.txt"
+
+ #define URL_fw_Version "https://raw.githubusercontent.com/augusto-salto/sensor_caixa_agua_v3/main/lib/versao/bin_version.txt"
+
+// FUNCIONAL --> #define URL_fw_Bin "https://raw.githubusercontent.com/augusto-salto/updateSensorNivel/main/fw.bin"
+
+// https://github.com/augusto-salto/sensor_caixa_agua_v3/blob/main/.pio/build/nodemcu-32s/firmware.bin?raw=true
+
+// https://github.com/augusto-salto/sensor_caixa_agua_v3/blob/main/.pio/build/nodemcu-32s/firmware.bin
+
+#define URL_fw_Bin "https://github.com/augusto-salto/sensor_caixa_agua_v3/blob/main/.pio/build/nodemcu-32s/firmware.bin?raw=true"
 
 extern TaskHandle_t handle_mqtt;
 extern TaskHandle_t handle_firebase;
